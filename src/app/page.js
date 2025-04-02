0824d1d9ca920b802183477bc0054384a279b94f
SuzaanAhmed <SuzaanAhmed00@gmail.com>
2025-04-02 17:32:30 +0530

    Finishing touch

--- src/app/page.js
+++ src/app/page.js
@@ -4,28 +4,36 @@ import { supabase } from "./lib/superbase";
 
 export default function Home() {
   const [StudentCount, setStudentCount] = useState([]);
-  useEffect(()=>{
+  useEffect(() => {
     loadStudentList();
-  },[]);
+  }, []);
 
   async function loadStudentList() {
-    const {data,error}=await supabase.from("student").select();
-    if(error) alert(JSON.stringify(error))
+    const { data, error } = await supabase.from("student").select();
+    if (error) alert(JSON.stringify(error))
 
     setStudentCount(data)
   }
 
   return (
-    <div className="flex flex-col items-center min-h-screen p-2">
-      {StudentCount.map((stud,index)=>{
-        return (
-          <div className="items-center justify-center content-center bg-gray-200 h-[150px] w-[400px] rounded m-2">
-            <h1 key={index}>Name: {stud.name}</h1>
-            <h1 key={stud.usn}>USN: {stud.usn}</h1>
-            <h1>Contact Details: {stud.phone}</h1>
+  <div className="flex justify-center items-center h-screen">
+    <div className="bg-red-200 flex flex-col w-[500px] items-center p-4 rounded-lg shadow-lg">
+      <strong className="text-lg text-gray-800 mb-3">Student Details</strong>
+      <div className="flex flex-col gap-3 w-full items-center">
+        {StudentCount.map((stud, index) => (
+          <div
+            key={index}
+            className="bg-gray-100 shadow-md h-auto w-[90%] rounded-lg p-3 border border-gray-300 text-center"
+          >
+            <h1 className="text-lg font-semibold text-gray-700">Name: {stud.name}</h1>
+            <h1 className="text-md text-gray-600">USN: {stud.usn}</h1>
+            <h1 className="text-md text-gray-600">Contact: {stud.phone}</h1>
           </div>
-        );
-      })}
+        ))}
+      </div>
     </div>
-    );
+  </div>
+  
+
+  );
 }
